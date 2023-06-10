13b7e445b28c595944608fb74ea3aabe00e22146
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-06-11 03:06:25 +0530

    feat: components

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,21 @@
 import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import CustomFilter from './CustomFilter';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
 
-export { Hero };
+export {
+	Hero,
+	CustomButton,
+	Navbar,
+	Footer,
+	CustomFilter,
+	SearchBar,
+	SearchManufacturer,
+	CarCard,
+	CarDetails,
+};
